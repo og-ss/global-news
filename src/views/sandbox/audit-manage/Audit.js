9de8b5fe1780f0d99492411575333d5008753a64
og-ss <1104449717@qq.com>
2022-10-11 16:52:09 +0800

    test

--- src/views/sandbox/audit-manage/Audit.js
+++ src/views/sandbox/audit-manage/Audit.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
 import React from 'react'
-import { useState,useEffect} from 'react'
-import { Table,Button,Space,notification} from 'antd'
-import { CheckOutlined,CloseOutlined } from '@ant-design/icons';
+import { useState, useEffect } from 'react'
+import { Table, Button, Space, notification } from 'antd'
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 
 
 function AuditNews() {
-  const { roleId, region,username } = JSON.parse(localStorage.getItem('token'))
-  const [dataSource,setdataSource]=useState([])
-  
+  const { roleId, region, username } = JSON.parse(localStorage.getItem('token'))
+  const [dataSource, setdataSource] = useState([])
+
   const roleObj = {
     "1": 'superadmin',
     "2": 'admin',
-    "3":'editor'
+    "3": 'editor'
   }
 
   const columns = [
@@ -20,7 +20,7 @@ function AuditNews() {
       title: '新闻标题',
       dataIndex: 'title',
       // 跳转至预览页面
-      render: (title,item) => <a href={`#/news-manage/preview/${item.id}`}>{title}</a>, 
+      render: (title, item) => <a href={`#/news-manage/preview/${item.id}`}>{title}</a>,
     },
     {
       title: '作者',
@@ -33,44 +33,44 @@ function AuditNews() {
     },
     {
       title: '操作',
-      render: (item) => 
-      <div>
-        <Space>
-          <Button type="primary" shape="circle" icon={<CheckOutlined />} onClick={()=>handleAudit(item,2,1)}></Button>
-          <Button danger type="primary" shape="circle" icon={<CloseOutlined />} onClick={()=>handleAudit(item,3,0)}></Button>
-        </Space>
-     </div>
-     },
+      render: (item) =>
+        <div>
+          <Space>
+            <Button type="primary" shape="circle" icon={<CheckOutlined />} onClick={() => handleAudit(item, 2, 1)}></Button>
+            <Button danger type="primary" shape="circle" icon={<CloseOutlined />} onClick={() => handleAudit(item, 3, 0)}></Button>
+          </Space>
+        </div>
+    },
   ]
 
-  useEffect(()=>{
+  useEffect(() => {
     getCurAuditNews()
-  },[])
-  
+  }, [])
+
   // 获取当前的新闻审核列表
   // auditState=1：审核中
-  const getCurAuditNews=()=>{
-    axios.get(`/news?auditState=1&_expand=category`).then(response=>{
-      const list=response.data
-       //如果是超级管理员roleId===1，获取所有的数据列表，如果不是，只能获取当前自己的角色列表以及同一个区域下的区域编辑用户
-       setdataSource(roleObj[roleId] === "superadmin" ? list : [
+  const getCurAuditNews = () => {
+    axios.get(`/news?auditState=1&_expand=category`).then(response => {
+      const list = response.data
+      //如果是超级管理员roleId===1，获取所有的数据列表，如果不是，只能获取当前自己的角色列表以及同一个区域下的区域编辑用户
+      setdataSource(roleObj[roleId] === "superadmin" ? list : [
         ...list.filter(item => item.username === username),
-        ...list.filter(item=>item.region===region&& roleObj[item.roleId]==='editor')
+        ...list.filter(item => item.region === region && roleObj[item.roleId] === 'editor')
       ])
     })
   }
-  
+
   //审核新闻
-  const handleAudit=(item,auditState,publishState)=>{
-    setdataSource(dataSource.filter(data=>data.id!==item.id))
-    axios.patch(`/news/${item.id}`,{
+  const handleAudit = (item, auditState, publishState) => {
+    setdataSource(dataSource.filter(data => data.id !== item.id))
+    axios.patch(`/news/${item.id}`, {
       auditState,
       publishState,
-    }).then(()=>{
+    }).then(() => {
       notification.info({
         message: '通知',
         description: `您可以到审核列表中查看新闻`,
-        placement:'bottomRight'
+        placement: 'bottomRight'
       });
     })
   }
@@ -78,10 +78,10 @@ function AuditNews() {
   return (
     <div>
       <Table dataSource={dataSource} columns={columns}
-      pagination={{
-      pageSize: 5,
-      }}
-      rowKey={item=>item.id}/>
+        pagination={{
+          pageSize: 5,
+        }}
+        rowKey={item => item.id} />
     </div>
   )
 }
