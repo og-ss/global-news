f28e2589b3b7597b0faf533be0686090216262b6
og-ss <1104449717@qq.com>
2022-12-07 11:39:37 +0800

    redux

---
+++ src/redux/story.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+
+const reducer = (prevState,action) => {
+    return prevState
+}
+
+const story = createStore();
+
+export default story;
--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button } from 'antd';
 
 export default function Home() {
+  com
   return (
     <div>
       <h1>首页</h1>
